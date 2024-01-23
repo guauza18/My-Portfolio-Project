fde3e68eb26df8ab66ebad68be51ebcbc4819fb6
guauza18 <113326468+guauza18@users.noreply.github.com>
2024-01-23 13:05:51 -0800

    Add files via upload

---
+++ capstone.js
@@ -0,0 +1,10 @@
+function validateForm(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert('Please fill out all fields.');
+        event.preventDefault();
+    }
+}
